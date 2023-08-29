7c2a850c1f5a0a0f486974163d535caf8aafd4c3
urdad <70736338+urdadx@users.noreply.github.com>
2023-08-29 20:56:10 +0000

    feat: updated landing page & added page titles✨

--- pages/admin/analytics/index.jsx
+++ pages/admin/analytics/index.jsx
@@ -7,7 +7,7 @@ const Analytics = () => {
 	return (
 		<>
 			<Head>
-				<title>Librelinks | Analytics</title>
+				<title>Librelinks | Analytics </title>
 			</Head>
 			<Layout>
 				<div className="w-full lg:w-[100vw] pl-4 pr-4 overflow-auto">
