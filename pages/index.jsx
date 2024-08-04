abb5a9b59f6dd48992bbf2649c011b0b1e0c8458
Yaman Devrim <yaman.devrim@icloud.com>
2024-08-05 01:50:19 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,268 +14,273 @@ export const metadata = {
 
 const Home = () => {
   const session = useSession();
-  const isAuthenticated = session.status === 'authenticated';
+  const isAuthenticated = session.status === 'authenticated' ? true : false;
 
   return (
     <>
-      <style jsx>{`
-        @import url('https://fonts.googleapis.com/css?family=Manrope:700|Manrope:400');
-
-        :root {
-          --text: hsl(0, 0%, 99%);
-          --textDim: hsl(0, 0%, 60%);
-          --background: hsl(0, 0%, 7%);
-          --primary: hsl(155, 100%, 65%);
-          --primaryBg: hsla(155, 100%, 65%, 1%);
-          --primaryHi: hsla(155, 100%, 75%, 25%);
-          --primaryFg: hsl(155, 100%, 85%);
-          --secondary: hsl(156, 51%, 14%);
-          --secondaryFg: hsl(156, 51%, 75%);
-          --secondaryBg: hsla(156, 51%, 14%, 5%);
-          --secondaryHi: hsla(156, 51%, 30%, 50%);
-          --accent: hsl(155, 100%, 94%);
-          --accentBg: hsla(155, 100%, 94%, 1%);
-          --accentHi: hsla(155, 100%, 100%, 25%);
-        }
-
-        body {
-          font-family: 'Manrope';
-          font-weight: 400;
-          background-color: var(--background);
-          color: var(--text);
-          padding: 0 10%;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          height: 100vh;
-          overflow: hidden;
-        }
-
-        nav {
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          gap: 2rem;
-          color: var(--textDim);
-          width: 100%;
-          box-sizing: border-box;
-          z-index: 9999;
-        }
-
-        .menu:hover {
-          color: var(--text);
-          cursor: pointer;
-        }
-
-        .sitename {
-          font-weight: bold;
-        }
-
-        .grid {
-          position: absolute;
-          height: 100%;
-          width: 100%;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          align-self: center;
-          z-index: -1;
-        }
-
-        .grid-svg {
-          height: 80%;
-          width: 80%;
-          position: relative;
-          z-index: 1;
-        }
-
-        .blur {
-          height: 12rem;
-          width: 12rem;
-          background-color: var(--primary);
-          filter: blur(100px);
-          border-radius: 100px;
-          z-index: 0;
-          position: absolute;
-        }
-
-        .title {
-          font-size: 10rem;
-          font-weight: 700;
-          letter-spacing: -0.6rem;
-          display: flex;
-          flex-direction: column;
-          position: absolute;
-          justify-content: center;
-          align-self: center;
-          height: 100%;
-          z-index: 1000;
-        }
-
-        .title > p {
-          margin: 0;
-          line-height: 10rem;
-          width: auto;
-        }
-
-        .title > p:nth-child(1) {
-          align-self: flex-start;
-        }
-
-        .title > p:nth-child(3) {
-          align-self: flex-end;
-        }
-
-        strong {
-          color: var(--primary);
-        }
-
-        .title > p:nth-child(2) {
-          align-self: flex-end;
-        }
-
-        .material-icons {
-          display: none;
-          fill: var(--text);
-        }
-
-        .button {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          position: absolute;
-          text-decoration: none;
-          color: inherit;
-          cursor: pointer;
-          height: 50px;
-          width: 160px;
-          z-index: 9999;
-        }
-
-        button {
-          height: 50px;
-          width: 160px;
-          clip-path: path('M 0 25 C 0 -5, -5 0, 80 0 S 160 -5, 160 25, 165 50 80 50, 0 55, 0 25');
-          border: none;
-          border-radius: 13px;
-          background-color: var(--primaryBg);
-          box-shadow: 0px -3px 15px 0px var(--primaryHi) inset;
-          color: var(--primaryFg);
-          font-family: 'Manrope';
-          font-size: 1rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          position: absolute;
-          transform: translateY(0px);
-          transition: all 0.2s ease;
-        }
-
-        span {
-          width: 100px;
-          height: 60px;
-          background-color: var(--primaryHi);
-          border-radius: 100%;
-          filter: blur(20px);
-          position: absolute;
-          bottom: -50%;
-          transition: all 0.2s ease;
-        }
-
-        .button:hover > span {
-          opacity: 60%;
-        }
-
-        .button:hover > button {
-          transform: translateY(5px);
-        }
-
-        .button.first {
-          top: 12%;
-          right: 20%;
-        }
-
-        .button.sec {
-          bottom: 13%;
-          right: 11%;
-        }
-
-        .button.sec > button {
-          background-color: var(--accentBg);
-          box-shadow: 0px -3px 15px 0px var(--accentHi) inset;
-          color: var(--accentFg);
-        }
-
-        .button.sec > span {
-          background-color: var(--accentHi);
-        }
-
-        .button.third {
-          bottom: 25%;
-          left: 15%;
-        }
-
-        .button.third > button {
-          background-color: var(--secondaryBg);
-          box-shadow: 0px -3px 15px 0px var(--secondary) inset;
-          color: var(--secondaryFg);
-        }
-
-        .button.third > span {
-          background-color: var(--secondaryHi);
-        }
-
-        .top-right {
-          position: absolute;
-          top: 0;
-          right: 0;
-          z-index: -1;
-          opacity: 50%;
-        }
-
-        .bottom-left {
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          z-index: -1;
-          opacity: 50%;
-        }
-
-        @media screen and (max-width: 1000px) {
-          .title {
-            font-size: 4rem;
-          }
-
-          .title > p {
-            line-height: 5rem;
-            letter-spacing: -0.3rem;
-          }
-
-          nav > :not(.sitename, .material-icons) {
-            display: none;
-          }
-
-          nav {
-            justify-content: space-between;
-          }
-
-          .material-icons {
-            display: flex;
-            align-items: center;
-          }
-        }
-      `}</style>
-
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-      </Head>
+        <title>Librelinks | A free & opensource link in bio tool</title>
+        {/* <!-- Open Graph (OG) meta tags --> */}
+        <meta property="og:url" content="https://librelinks.vercel.app/" />
+        <meta property="og:url" content="https://librelinks.me/" />
+        <meta property="og:url" content="https://www.librelinks.me/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:site_name"
+          content="Librelinks - The free & opensource link in bio tool"
+        />
+        <meta property="og:title" content="Librelinks" />
+        <meta
+          property="og:description"
+          content="Librelinks is an opensource link in bio tool that helps you easily manage your links, transforming your online presence."
+        />
+        <meta
+          property="og:image"
+          itemprop="image"
+          content="https://librelinks.vercel.app/og.png"
+        />
+        <meta
+          property="og:image"
+          itemprop="image"
+          content="https://librelinks.me/og.png"
+        />
+        <meta
+          property="og:image"
+          itemprop="image"
+          content="https://www.librelinks.me/og.png"
+        />
 
-      <div className="grid">
-        <svg className="grid-svg" xmlns="http://www.w3.org/2000/svg" width="982" height="786" viewBox="0 0 982 786" fill="none">
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M490 401V537H348.5V401H490ZM490 785.5V676H348.5V785.5H347.5V676H206V785.5H205V676H63
+        {/* <!-- Twitter Card meta tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@urdadx" />
+        <meta name="twitter:creator" content="@urdadx" />
+        <meta
+          property="twitter:domain"
+          content="https://librelinks.vercel.app/"
+        />
+        <meta property="twitter:domain" content="https://librelinks.me/" />
+        <meta property="twitter:domain" content="https://www.librelinks.me/" />
+        <meta property="twitter:url" content="https://librelinks.vercel.app/" />
+        <meta name="twitter:title" content="Librelinks" />
+        <meta
+          name="twitter:description"
+          content="Librelinks is an opensource link in bio tool that helps you easily manage your links, transforming your online presence."
+        />
+        <meta
+          name="twitter:image"
+          content="https://librelinks.vercel.app/og.png"
+        />
+        <meta name="twitter:image" content="https://librelinks.me/og.png" />
+        <meta name="twitter:image" content="https://www.librelinks.me/og.png" />
+        <meta
+          data-rh="true"
+          name="twitter:image:alt"
+          content="Librelinks is an opensource link in bio tool that helps you easily manage your links, transforming your online presence."
+        />
+
+        {/* <!-- LinkedIn meta tags --> */}
+        <meta
+          property="og:linkedin:image"
+          content="https://librelinks.vercel.app/og.png"
+        />
+        <meta
+          property="og:linkedin:image"
+          content="https://librelinks.me/og.png"
+        />
+        <meta
+          property="og:linkedin:image"
+          content="https://www.librelinks.me/og.png"
+        />
+        <meta property="og:linkedin:title" content="Librelinks" />
+        <meta
+          property="og:linkedin:description"
+          content="Librelinks is an opensource link in bio tool that helps you easily manage your links, transforming your online presence."
+        />
+
+        {/* <!-- Facebook meta tags --> */}
+        <meta
+          property="og:facebook:image"
+          content="https://librelinks.vercel.app/og.png"
+        />
+        <meta
+          property="og:facebook:image"
+          content="https://librelinks.me/og.png"
+        />
+        <meta
+          property="og:facebook:image"
+          content="https://www.librelinks.me/og.png"
+        />
+        <meta property="og:facebook:title" content="Librelinks" />
+        <meta
+          property="og:facebook:description"
+          content="Librelinks is an opensource link in bio tool that helps you easily manage your links, transforming your online presence."
+        />
+
+        {/* <!-- Instagram meta tags --> */}
+        <meta
+          property="og:instagram:image"
+          content="https://librelinks.vercel.app/og.png"
+        />
+        <meta
+          property="og:instagram:image"
+          content="https://librelinks.me/og.png"
+        />
+        <meta
+          property="og:instagram:image"
+          content="https://www.librelinks.me/og.png"
+        />
+        <meta property="og:instagram:title" content="Librelinks" />
+        <meta
+          property="og:instagram:description"
+          content="Librelinks is an opensource link in bio tool that helps you easily manage your links, transforming your online presence."
+        />
+
+        {/* <!-- Pinterest meta tags --> */}
+        <meta
+          property="og:pinterest:image"
+          content="https://librelinks.vercel.app/og.png"
+        />
+        <meta
+          property="og:pinterest:image"
+          content="https://librelinks.me/og.png"
+        />
+        <meta
+          property="og:pinterest:image"
+          content="https://www.librelinks.me/og.png"
+        />
+        <meta property="og:pinterest:title" content="Librelinks" />
+        <meta
+          property="og:pinterest:description"
+          content="Librelinks is an opensource link in bio tool that helps you easily manage your links, transforming your online presence."
+        />
+      </Head>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+        <div className="relative overflow-hidden">
+          <div
+            className="absolute inset-y-0 w-full h-full"
+            aria-hidden="true"
+          ></div>
+          <div className="relative pt-6 pb-16 sm:pb-24">
+            <div className="px-4 mx-auto max-w-7xl sm:px-6">
+              <nav
+                className="relative flex items-center justify-between md:justify-start"
+                aria-label="Global"
+              >
+                <Link
+                  className="flex items-center gap-2 font-bold text-xl"
+                  href="/"
+                >
+                  <h3 className="lg:block">Librelinks</h3>
+                </Link>
+
+                <div className="relative items-center w-28 z-10 md:absolute md:inset-y-0 md:right-0">
+                  <Link
+                    className="group inline-flex items-center gap-2 px-4 text-sm  bg-slate-900 border rounded-3xl text-white w-[116px] h-[35px] justify-center font-semibold transition-colors hover:bg-slate-700"
+                    rel="noopener noreferrer"
+                    href="/admin"
+                  >
+                    {isAuthenticated ? 'Admin' : 'Login'}
+                  </Link>
+                </div>
+              </nav>
+            </div>
+            <div className="px-4 mx-auto mt-24 max-w-7xl sm:mt-16 sm:px-6">
+              <div className="flex justify-center items-center mb-6">
+                <a
+                  className="group inline-flex items-center gap-2 px-4 py-4 text-sm bg-gray-50 border rounded-3xl text-gray-500 w-[180px] h-[35px] justify-center transition-colors hover:bg-gray-100"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/urdadx/librelinks"
+                >
+                  <div className="">
+                    <GithubStar />
+                  </div>{' '}
+                  Star us on Github
+                </a>
+              </div>
+              <div className="text-center">
+                <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+                  <span className="block">The free & opensource</span>
+                  <span className="hero-title block ">link in bio tool</span>
+                </h1>
+                <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                  Librelinks is an opensource link in bio tool that helps you
+                  easily manage your links, transforming your online presence.
+                </p>
+              </div>
+              <div className="flex justify-center mt-6">
+                <div className="flex flex-col items-center">
+                  <span className="inline-flex rounded-xl shadow">
+                    <Link legacyBehavior href="/register">
+                      <a className="inline-flex items-center px-4 py-2 font-medium text-lg gradient-btn border border-transparent rounded-xl text-white w-[190px] h-[50px] justify-center hover:shadow-lg">
+                        Get started
+                      </a>
+                    </Link>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="absolute inset-0 flex flex-col" aria-hidden="true">
+              <div className="flex-1" />
+              <div className="flex-1 w-full bg-slate-900 " />
+            </div>
+            <div className="px-4 mx-auto max-w-7xl sm:px-6">
+              <Image
+                className="relative rounded-lg shadow-lg"
+                src="/assets/new_shot.png"
+                alt="App screenshot"
+                height={700}
+                width={1200}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="bg-slate-900">
+          <div className="px-4 py-16 mx-auto max-w-7xl sm:py-24 sm:px-6 lg:px-8">
+            <h2 className="text-lg font-semibold tracking-wide text-center text-gray-400">
+              Made by{' '}
+              <a
+                className="hover:text-emerald-500"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://twitter.com/NerdyProgramme2"
+              >
+                @urdadx
+              </a>
+            </h2>
+            <div className="flex items-center gap-4 justify-center mt-4">
+              <a
+                href="https://x.com/NerdyProgramme2"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="twitter logo"
+              >
+                <TwitterIcon color="white" />
+              </a>
+              <a
+                href="https://github.com/urdadx"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="github logo"
+              >
+                <GithubIcon color="white" />
+              </a>
+              <a
+                href="https://urdadx.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="insta logo"
+              >
+                <GlobeIcon color="white" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
