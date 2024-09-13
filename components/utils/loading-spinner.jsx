0f36d0ec1744e88b7b936e641bbe77aefe0b80f7
yyamandevrim <yaman.devrim@icloud.com>
2024-09-13 13:11:08 +0300

    change: revert old loader

--- components/utils/loading-spinner.jsx
+++ components/utils/loading-spinner.jsx
@@ -1,13 +1,30 @@
-// /components/utils/loading-spinner.jsx
-
 import React from 'react';
-import styles from './loading-spinner.module.css'; // Import the CSS module
+import { Oval } from 'react-loader-spinner';
 
-const Loader = ({ bgColor, textColor, message, width, height, strokeWidth }) => {
+
+const Loader = ({
+  bgColor,
+  textColor,
+  message,
+  width,
+  height,
+  strokeWidth,
+}) => {
   return (
     <>
       <div className="flex justify-center mt-10 mx-auto">
-        <div className={styles.loader}></div> {/* Use the class from the module */}
+        <Oval
+          height={height ? height : 70}
+          width={width ? width : 70}
+          color={bgColor}
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="oval-loading"
+          secondaryColor={bgColor}
+          strokeWidth={strokeWidth ? strokeWidth : 2}
+          strokeWidthSecondary={2}
+        />
       </div>
       <div className="flex justify-center mx-auto">
         <h3 className={`${textColor} font-semibold text-l`}>{message}</h3>
@@ -17,3 +34,5 @@ const Loader = ({ bgColor, textColor, message, width, height, strokeWidth }) => 
 };
 
 export default Loader;
+
+/* HTML: <div class="loader"></div> */
