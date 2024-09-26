1c6812cc36bdc599d38e472d26614952b65e0dbf
yyamandevrim <yaman.devrim@icloud.com>
2024-09-26 18:23:29 +0300

    add: redirect to spotify playlist

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/spotify',
+        destination: 'https://open.spotify.com/playlist/2dN9BfIi1ztGcOPOpwoZqG',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
